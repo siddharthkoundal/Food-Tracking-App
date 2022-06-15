928f9bb133507943ef7c256cc19a59edefc57ef3
siddharthkoundal <sksiddharth27@gmail.com>
2022-06-15 17:10:40 +0530

    home page

---
+++ src/js/app.js
@@ -0,0 +1,1 @@
+console.log("SSU from app");
---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+let mix = require("laravel-mix");
+
+mix
+  .js("src/js/app.js", "public/js/app.js")
+  .sass("src/scss/app.scss", "public/css/app.css");

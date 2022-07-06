500538d54396425a7907be8fef5ec354482e719a
siddharthkoundal <sksiddharth27@gmail.com>
2022-07-06 15:06:58 +0530

    socket integration

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,14 @@
 const authController = require("../app/http/controllers/authController");
 const cartController = require("../app/http/controllers/customers/cartController");
 const homeController = require("../app/http/controllers/homeController");
+const orderController = require("../app/http/controllers/customers/orderController");
+const AdminOrderController = require("../app/http/controllers/admin/orderController");
+const statusController = require("../app/http/controllers/admin/statusController");
+
+//Middlewares
+const auth = require("../app/http/middlewares/auth");
+const admin = require("../app/http/middlewares/admin");
+const guest = require("../app/http/middlewares/guest");
 
 function initRoutes(app) {
   app.get("/", homeController().index);
@@ -9,9 +17,31 @@ function initRoutes(app) {
 
   app.post("/update-cart", cartController().update);
 
-  app.get("/login", authController().login);
+  app.get("/login", guest, authController().login);
+
+  app.post("/login", guest, authController().postLogin);
 
   app.get("/register", authController().register);
+
+  app.post("/register", authController().postRegister);
+
+  app.post("/logout", authController().logout);
+
+  app.post("/orders", orderController().store);
+
+  //Customer routes
+
+  app.post("/orders", auth, orderController().store);
+
+  app.get("/customer/orders", auth, orderController().index);
+
+  app.get("/customer/orders/:id", auth, orderController().show);
+
+  //Admin Routes
+
+  app.get("/admin/orders", admin, AdminOrderController().index);
+
+  app.post("/admin/order/status", admin, statusController().update);
 }
 
 module.exports = initRoutes;
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongo");
+const passport = require("passport");
+const Emitter = require("events");
 
 //Database Connection
 const url = "mongodb://localhost/pizza";
@@ -25,11 +27,15 @@ connection
     console.log(err);
   });
 
-//Session store
+// Session store
 let mongoStore = MongoDbStore.create({
   mongoUrl: url,
   collection: "sessions",
 });
+
+//Event emitter
+const eventEmitter = new Emitter();
+app.set("eventEmitter", eventEmitter);
 
 //Session config
 app.use(
@@ -42,14 +48,33 @@ app.use(
   })
 );
 
+// Passport Config
+
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "hello",
+  })
+);
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 //Assets
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Global middleware
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 });
 
@@ -60,6 +85,24 @@ app.set("view engine", "ejs");
 
 require("./routes/web")(app);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+});
+
+//Socket
+
+const io = require("socket.io")(server);
+io.on("connection", (socket) => {
+  //Join
+  socket.on("join", (roomName) => {
+    socket.join(roomName);
+  });
+});
+
+eventEmitter.on("orderUpdated", (data) => {
+  io.to(`order_${data.id}`).emit("orderUpdated", data);
+});
+
+eventEmitter.on("orderPlaced", (data) => {
+  io.to("adminRoom").emit("orderPlaced", data);
 });
